559cc34d38d0a9e97c631201448f178a1455742e
Romeo Dixon <romeodixon2nd@gmail.com>
2022-02-19 15:32:21 -0600

    updated del route and added readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # E-Commerce-Back-End
+![GitHub license](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+This application is a backend application servicing front end routes and using MySQL database with Node.Js, Express, Sequelize and dotenv.
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#Contributing)
+- [Test](#Test)
+- [Questions](#Questions)
+
+## Installation
+Cloning the repo from Github. Then install Node.js. Run "npm i" in terminal to get packages.
+
+## Usage
+This application is running under mysql as a local host, you can modify the .env file with your own user/password to start the application.
+
+## License
+MIT
+Info on license: https://opensource.org/licenses/MIT
+
+## Contributing 
+Contributors are welcome. Installation above.
+
+## TestTest
+npm test
+
+## Questions 
+Email me at: romeodixonll@yahoo.com 
+Find me on Github: https://github.com/romeodixonll
+
+
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,42 +1,42 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async(req, res) => {
+router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{model: Category}, {model: Tag}]
-    })
+      include: [{ model: Category }, { model: Tag }],
+    });
 
-    res.status(200).json(productData)
+    res.status(200).json(productData);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 });
 
 // get one product
-router.get('/:id', async(req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include:[{model:Category}, {model:Tag}]
-    })
-    if(!productData){
-      res.status(404).json({message:"No Product Found"})
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No Product Found" });
     }
-    res.status(200).json(productData)
+    res.status(200).json(productData);
   } catch (error) {
-    res.statut(500).json(error)
+    res.statut(500).json(error);
   }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -68,7 +68,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -109,22 +109,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async(req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData  = await Product.destroy({
-      where: req.params.id
-    })
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-    if(!productData){
+    if (!productData) {
       res.status(404).json({
-        message: "No Product Found"
-      })
+        message: "No Product Found",
+      });
     }
 
-    res.status(200).json(productData)
+    res.status(200).json(productData);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 });
 
